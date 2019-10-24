8ce53da4c991e51b0a65befc72c716031deb4de0
a.bogdanov <a.bogdanov@evotor.ru>
2019-10-24 16:44:25 +0300

    init

---
+++ src/uk/flood/log/AtomicLock.java
@@ -0,0 +1,19 @@
+package uk.flood.log;
+
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+final class AtomicLock extends AtomicBoolean {
+    AtomicLock() {
+        super(false);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    void lock() {
+        while (!compareAndSet(false, true)) ;
+    }
+
+    void unlock() {
+        set(false);
+    }
+}
